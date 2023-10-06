a30b6cebcdac338ee6fd6d5b33bd6cd4fd4a8325
avila-ca <avilacalaf@gmail.com>
2023-10-06 19:17:00 +0200

    .env y dotenv creados

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
+
 import { Server } from "./Server";
 
+dotenv.config();
 export class App {
 	server?: Server;
 
 	async start(): Promise<void> {
-		const port = process.env.PORT ?? "8000";
+		const port = process.env.PORT ?? "3000";
 		this.server = new Server(port);
 
 		await this.server.listen();
